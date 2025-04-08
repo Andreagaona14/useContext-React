74ef78cca00f0b6bb9d493686d172bce8d6b220d
Andrea gaona 14 <andreitagaona05@gmail.com>
2025-04-08 11:17:44 -0500

    Cambiosn en los estilos

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,16 @@ export const App = () => {
   
   return (
     <>
-      <div className='max-w-md mx-auto mt-10 p-6 bg-white rounded-lg shadow-md border-1 border-gray-200'>
+    <main className='flex items-center justify-center min-h-screen bg-blue-200'>
+    <div className='max-w-md mx-auto mt-10 p-6 bg-white rounded-lg shadow-md border-1 border-gray-200'>
       <h1 className='text-2xl font-bold text-center mb-6' >To-Do List</h1>
 
       <Form/>
       <Filters/>
       <List/>
       </div>
+    </main>
+      
      </>
     
   )
